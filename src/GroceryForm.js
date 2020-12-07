9fde96beee7475a0f07677d06c990e4561619bf0
Joshua Parco <jacparco@gmail.com>
2020-12-07 16:38:55 -0700

    added storing form data to state

--- src/GroceryForm.js
+++ src/GroceryForm.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class GroceryForm extends React.Component {
+  state = { name: '' }
+
+  handleChange = (event) => {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.name)
+  }
+
   render () {
+    const { name } = this.state;
+
     return (
-      <form>
-        <input required placeholder="Add An Item" />
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          value={name}
+          name="name"
+          onChange={this.handleChange}
+          required
+          placeholder="Add An Item"
+         />
       </form>
     )
   }
